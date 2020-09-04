f0950f01e47df00696833641c828a17040ba6d1a
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-04 03:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,18 +58,16 @@ let wagesEarnedOnDate = function(employee, dateSought){
     return parseFloat(rawWage.toString())
 }
 
-let allWagesFor = function(employeeObject) {
-  let eligibleDates = employeeObject.timeInEvents.map(function(event) {
-    return event.date;
-  })
-  //employeeObject.timeInEvents.map(event => event.date)
-  
-   let payable = eligibleDates.reduce(function(memo, d){
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
         return memo + wagesEarnedOnDate(employee, d)
     }, 0)
 
     return payable
-  
 }
 
 let findEmployeeByFirstName = function(srcArray, firstName) {
