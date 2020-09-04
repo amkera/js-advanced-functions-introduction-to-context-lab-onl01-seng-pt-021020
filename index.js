3fd4fb6e137edb812c3f041848cb080b371f7b1d
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-04 03:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+let createEmployeeRecord = function(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2], 
+    payPerHour: array[3],
+    timeInEvents: [], 
+    timeOutEvents: []
+  }
+}; //input is an array, and output is an object 
+
+
+
+let createEmployeeRecords = function(arrayOfArrays) {
+  return arrayOfArrays.map(array => createEmployeeRecord(array))
+}; //input is an array of arrays, and output is an array of objects
+
+let createTimeInEvent = function(employeeObject, dateStamp) {
+  let [date, hour] = dateStamp.split(" ")
+  employeeObject.timeInEvents.push({
+    type: "TimeIn", 
+    hour: parseInt(hour, 10),
+    date: date
+  })
+  return employeeObject;
+  
+}
+
+let createTimeOutEvent = function(employeeObject, dateStamp) {
+  let [date, hour] = dateStamp.split(" ")
+  employeeObject.timeInEvents.push({
+    type: "TimeIn", 
+    hour: parseInt(hour, 10),
+    date: date
+  })
+  return employeeObject;
+  
+}
+
+
